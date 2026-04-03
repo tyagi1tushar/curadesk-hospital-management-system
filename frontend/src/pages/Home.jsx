23ae4a658c557b3bb2ea4ca9bd3a12b774eadedb
tushar28rv <anjutushartyagi2810@gmail.com>
2026-04-03 16:54:29 +0530

    added benner to frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
+import Banner from '../components/Banner';
 
 const Home = () => {
     return (
        <div className="min-h-screen bg-white bg-white transition-colors duration-300">
             <Navbar />
+            <Banner />
         </div>
     );
 };
